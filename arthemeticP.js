8d32118939aefb543ae1ad5af7ed81105078005e
Sohail5158 <94858933+Sohail5158@users.noreply.github.com>
2021-12-26 17:58:30 +0530

    Add files via upload

---
+++ arthemeticP.js
@@ -0,0 +1,16 @@
+var num1=prompt("enter first number")
+num1=parseInt(num1)
+var num2=prompt("enter common difference number")
+num2=parseInt(num2)
+var n=prompt("enter nth term")
+n=parseInt(n)
+ 
+for(var i=1;i<=n;i++){
+  
+ var z=(i-1)*num2
+var y=num1+z
+console.log(y)
+ 
+
+
+}
